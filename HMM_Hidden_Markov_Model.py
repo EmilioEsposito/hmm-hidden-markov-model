aafe55adf760205731b4c84e0dfcc30053bf5d06
Emilio Esposito <espo412@gmail.com>
2016-07-27 15:55:47 -0400

    faster version using array algebra

--- HMM_Hidden_Markov_Model.py
+++ HMM_Hidden_Markov_Model.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jul 13 15:45:39 2016
+Created on Wed Jul 27 15:53:23 2016
 
 @author: Emilio Esposito
 All probabilities calculated use log-probabilities, EXCEPT for the inital dfs: emit, trans, prior
@@ -8,9 +8,9 @@ All probabilities calculated use log-probabilities, EXCEPT for the inital dfs: e
 
 import pandas as pd
 import numpy as np
-from logsum import log_sum_lst
+from scipy.misc import logsumexp
 
-# INPUTS
+# INPUTS 
 emit = pd.read_csv("emit.csv",index_col=0)
 trans = pd.read_csv("trans.csv",index_col=0)
 prior = pd.read_csv("prior.csv",index_col=0)
@@ -30,21 +30,23 @@ print "original trans", trans
 def forward(prior, trans, emit, states, obs):
     # create trellis like trellis[t][state] e.g. trellis[0]["s1"]
     trellis = pd.DataFrame(np.zeros([len(states),len(obs)]), index=states, columns=range(len(obs)))
-
+    
     for t in trellis.columns[:-1]:
         if t == 0:
             trellis.loc[:,t] = np.log(prior.loc[:,"prob"]) + np.log(emit.loc[:, obs[t]])
         
+#        trellis.loc[:, t+1] = np.log(emit.loc[:, obs[t+1]]) + logsumexp(trellis.loc[:,t] + np.log(trans.loc[:,i_state]))
         for i_state in states:
             
-            log_summation = log_sum_lst([trellis.loc[j_state,t] + np.log(trans.loc[j_state,i_state]) for j_state in states])
+            log_summation = logsumexp(trellis.loc[:,t] + np.log(trans.loc[:,i_state]))
             
             trellis.loc[i_state, t+1] = np.log(emit.loc[i_state, obs[t+1]]) + log_summation
     
+    
     # get total prob
     # sum the last column across all states
-    total_prob = log_sum_lst(trellis.loc[:,trellis.columns[-1:]].values)
-    print "fwd", total_prob[0]
+    total_prob = logsumexp(trellis.loc[:,trellis.columns[-1:]].values)
+    print "fwd", total_prob
     
     return trellis, total_prob
       
@@ -58,12 +60,12 @@ def backward(prior, trans, emit, states, obs):
         else:
             for i_state in states:
                 
-                log_summation = log_sum_lst([trellis.loc[j_state,t+1] + np.log(trans.loc[i_state,j_state]) + np.log(emit.loc[j_state, obs[t+1]]) for j_state in states])
+                log_summation = logsumexp(trellis.loc[:,t+1] + np.log(trans.loc[i_state,:]) + np.log(emit.loc[:, obs[t+1]]))
                 
                 trellis.loc[i_state, t] = log_summation
     
     # get total prob
-    total_prob = log_sum_lst([np.log(prior.loc[i_state,"prob"]) + np.log(emit.loc[i_state, obs[0]]) + trellis.loc[i_state, 0] for i_state in states])
+    total_prob = logsumexp(np.log(prior.loc[:,"prob"]) + np.log(emit.loc[:, obs[0]]) + trellis.loc[:, 0])
     print "bkwd", total_prob
     
     return trellis
@@ -84,7 +86,7 @@ def viterbi(prior, trans, emit, states, obs):
         
         for i_state in states:
             
-            max_prob = np.max([trellis.loc[j_state,t] + np.log(trans.loc[j_state,i_state]) for j_state in states])
+            max_prob = np.max(trellis.loc[:,t] + np.log(trans.loc[:,i_state]))
             
             trellis.loc[i_state, t+1] = np.log(emit.loc[i_state, obs[t+1]]) + max_prob
             path.loc[i_state, t+1] = trellis.loc[:,t+1].idxmax()
@@ -103,59 +105,39 @@ def fwd_bwd():
     
     # calculate Xi (aka ksi)
     # Probability of transiting from to Si to Sj at time t given output O
-    def ksi(t, i_state, j_state):
-        # calc ksi_t_ij using log prob
-        # Probability of transiting from to Si to Sj at time t given output O
-        ksi_t_ij = f_trellis.loc[i_state,t] + np.log(trans.loc[i_state, j_state]) + np.log(emit.loc[j_state, obs[t+1]]) + b_trellis.loc[j_state, t+1] - total_prob
-        return ksi_t_ij[0]
+    # make 3D ksi array ksi_arr[t,i,j]
+    ksi_arr = np.array(np.zeros([len(obs),len(states), len(states)]))
     
-    # update trans matrix A
-    for i_state in states:
-        # initialize numerators and denominator
-        aij_numerator_lst = []
-        aij_denominator = np.log(0)
-        
-        # calc numerators, and accumulate numerators into a denominator
-        for j_state in states:
-            # calc numerator for j_state
-            aij_numerator = log_sum_lst([ksi(t, i_state, j_state) for t in list(obs.index)[:-1]])
-            # add it to a list, we'll use it next loop
-            aij_numerator_lst.append(aij_numerator)
-            # the running total of numerators becomes the denominator 
-            aij_denominator = log_sum_lst([aij_denominator, aij_numerator])
-        # update trans 
-        for j_state, aij_numerator in zip(states, aij_numerator_lst):
+    # fill the ksi_arr
+    for t in range(len(obs)-1):
+        for i, i_state in enumerate(states):
+            ksi_arr[t,i,:] = f_trellis.loc[i_state,t] + np.log(trans.loc[i_state, :]) + np.log(emit.loc[:, obs[t+1]]) + b_trellis.loc[:, t+1] - total_prob
             
-            # update trans probs and convert from log prob to normal prob by using exponentiation
-            trans.loc[i_state, j_state] = np.exp(aij_numerator - aij_denominator)
-    
-    # update emissions emit matrix B
-    for j_state in states:
-        # initialize numerators and denominator
-        bij_numerator_lst = []
-        bij_denominator = np.log(0)
-        
-        # calculate denominator        
-        ksi_t_ij_lst = [log_sum_lst([ksi(t, i_state, j_state) for i_state in states]) for t in list(obs.index)[:-1]]
-        bij_denominator = log_sum_lst(ksi_t_ij_lst)
-        
-        # loop through all k emission output possibilities
-        for k in emit.columns:
-            # calculate numerator
-            bij_numerator_lst = [np.log(0)]
-            
-            # calculate ksi where k == observed output and store it in a list
-            for t in range(len(obs)-1):
-                if obs.loc[t+1] == k:
-                    for i_state in states:
-                        bij_numerator_lst.append(ksi(t, i_state, j_state))
-                        
-            # sum the list to get the numerator value
-            bij_numerator = log_sum_lst(bij_numerator_lst)
-            
-            # update emission probs and convert from log prob to normal prob by using exponentiation
-            emit.loc[j_state, k] = np.exp(bij_numerator - bij_denominator)
-            
+    # update trans matrix A            
+    for i, i_state in enumerate(states):
+       trans_denominator = logsumexp(ksi_arr[:,i,:].flatten())
+       for j, j_state in enumerate(states):
+           trans_numerator = logsumexp(ksi_arr[:,i,j].flatten())
+           # update trans probs and convert from log prob to normal prob by using exponentiation
+           trans.loc[i_state, j_state] = np.exp(trans_numerator - trans_denominator)
+
+    # update emit matrix B            
+    for j, j_state in enumerate(states):
+       emit_denominator = logsumexp(ksi_arr[:,:,j].flatten())
+       
+       # loop through all k emission output possibilities
+       for k in emit.columns:
+           emit_numerator_arr = np.array(np.log(0)) 
+           # sum ksi where k == observed output and store it in numerator
+           for t in obs.index:
+               if obs.loc[t] == k:
+                   emit_numerator_arr = np.append(emit_numerator_arr, ksi_arr[t,:,j].flatten())
+           # sum the list to get the numerator value
+           emit_numerator = logsumexp(emit_numerator_arr.flatten())
+           
+           # update emission probs and convert from log prob to normal prob by using exponentiation
+           emit.loc[j_state, k] = np.exp(emit_numerator - emit_denominator)
+  
     # return total_prob so that we can test for convergance between consecutive runs
     return total_prob
 
@@ -165,10 +147,10 @@ total_prob = np.log(1)
 
 # TRAINING
 # run fwd-bwd algo until the probabilies converge
-i = 0
-while np.abs(last_total_prob - total_prob) > .1:
-    i+=1
-    print "Iteration #", i, " of fwd-bwd algo..."
+iteration = 0
+while np.abs(last_total_prob - total_prob) > .01:
+    iteration+=1
+    print "Iteration #", iteration, " of fwd-bwd algo..."
     last_total_prob = total_prob
     total_prob = fwd_bwd()
 
